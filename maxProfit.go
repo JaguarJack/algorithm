33ff3a1a71086568c5c314f3990c303457bc20ed
JaguarJack <njphper@gmail.com>
2020-07-30 09:14:18 +0800

    买股票

--- maxProfit.go
+++ maxProfit.go
@@ -1,1 +1,38 @@
 package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println(maxProfit([]int{2,1,2,0,1}))
+}
+
+
+func maxProfit(prices []int) int {
+	var i, max int
+
+	start := -1
+
+	l := len(prices)
+
+	//var temp []int
+	for i = 0; i < l; i++ {
+		if i < l-1 && prices[i] < prices[i+1] {
+			if start == -1 {
+				start = prices[i]
+			}
+
+			if start > prices[i] {
+				start = prices[i]
+			}
+		} else {
+
+			if start >= 0 {
+				max += prices[i] - start
+			}
+			start = -1
+		}
+	}
+
+	return max
+}
+
