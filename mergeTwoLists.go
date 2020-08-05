ee8921224d3dacd2832085075e4eb44e74d06e63
JaguarJack <njphper@gmail.com>
2020-08-05 08:42:18 +0800

    合并两个连表

--- mergeTwoLists.go
+++ mergeTwoLists.go
@@ -1,1 +1,34 @@
 package main
+
+func main()  {
+
+}
+
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+		pre := &ListNode{}
+
+		result := pre
+
+		for l1 != nil && l2 != nil {
+			if l1.Val < l2.Val {
+				pre.Next = l1
+				l1 = l1.Next
+			} else {
+				pre.Next = l2
+				l2.Next = l2
+			}
+
+			pre = pre.Next
+		}
+
+		if l1 != nil {
+			pre.Next = l1
+		}
+
+		if l2 != nil {
+			pre.Next = l2
+		}
+
+		return result.Next
+}
